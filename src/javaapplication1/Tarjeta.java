ac490824abc8c95c128a8dce133bf38e8d6d96fe
Wílmer E. León Díaz <42726764+wilmereleon@users.noreply.github.com>
2023-06-20 19:40:13 -0500

    Indentanción de la clase Tarjeta
    Se organizan los comentarios y la indentación general de la clase.

--- src/javaapplication1/Tarjeta.java
+++ src/javaapplication1/Tarjeta.java
@@ -65,105 +65,106 @@ public class Tarjeta {
     public boolean isShown() {
         return boton.getText().equals("✔");
     }
-  /**
-   * Set the value of tema
-   * @param tema the new value of tema
-   */
-  public void setTema (int tema) {
+    /**
+    * Set the value of tema
+    * @param tema the new value of tema
+    */
+    public void setTema (int tema) {
         tema = tema;
-  }
+    }
 
-  /**
-   * Get the value of tema
-   * @return the value of tema
-   */
-  public int getTema () {
-    return tema;
-  }
+    /**
+    * Get the value of tema
+    * @return the value of tema
+    */
+    public int getTema () {
+        return tema;
+    }
   
-  public int getPareja() {
+    public int getPareja() {
         return pareja;
     }
 
-  /**
-   * Set the value of imagen
-   * @param imagen the new value of imagen
+    /**
+    * Set the value of imagen
+    * @param imagen the new value of imagen
+    */
+    public void setImagen (String imagen) {
+        imagen = imagen;
+    }
+
+    /**
+    * Get the value of imagen
+    * @return the value of imagen
+    */
+    public String getImagen () {
+        return imagen;
+    }
+
+    /**
+    * Set the value of estadoDeTarjeta
+    * @param oculta visible acertada the new value of estadoDeTarjeta
+    */
+    public void setEstadoDeTarjeta (int oculta, int visible, int acertada) {
+    /**
+    * @autor: Wílmer E. León
+    * Comentario: poner código de estados
    */
-  public void setImagen (String imagen) {
-    imagen = imagen;
-  }
+        estadoDeTarjeta = visible;
+    }
 
-  /**
-   * Get the value of imagen
-   * @return the value of imagen
-   */
-  public String getImagen () {
-    return imagen;
-  }
+    /**
+    * Get the value of estadoDeTarjeta
+    * @return the value of estadoDeTarjeta
+    */
+    public int getEstadoDeTarjeta () {
+        return estadoDeTarjeta;
+    }
 
-  /**
-   * Set the value of estadoDeTarjeta
-   * @param oculta visible acertada the new value of estadoDeTarjeta
-   */
-  public void setEstadoDeTarjeta (int oculta, int visible, int acertada) {
-   /**
-   * @autor: Wílmer E. León
-   * Comentario: poner código de estados
-   */
-    estadoDeTarjeta = visible;
-  }
+    //
+    // Other methods
+    //
 
-  /**
-   * Get the value of estadoDeTarjeta
-   * @return the value of estadoDeTarjeta
-   */
-  public int getEstadoDeTarjeta () {
-    return estadoDeTarjeta;
-  }
-
-  //
-  // Other methods
-  //
-
-  /**
-   * @param estadoDeTarjeta
-   * @param imagen
-   */
-  public void Tarjeta (int estadoDeTarjeta, String imagen) {
-      /**
-   * @autor: Wílmer E. León
-   * Comentario: poner código de Tarjeta
-   */
-  }
+    /**
+    * @param estadoDeTarjeta
+    * @param imagen
+    */
+    public void Tarjeta (int estadoDeTarjeta, String imagen) {
+    /**
+    * @autor: Wílmer E. León
+    * Comentario: poner código de Tarjeta
+    */
+    }
 
 
-  /**
-   * @return int
-   */
-  public int getTema (int tema) {
-      return tema;
-  }
+    /**
+    * @return int
+    */
+    public int getTema (int tema) {
+        return tema;
+    }
 
 
-  /**
-   * @return       String
-   */
-  public String getImagen (String imagen) {
-      return imagen;
-  }
+    /**
+    * @return       String
+    */
+    public String getImagen (String imagen) {
+        return imagen;
+    }
 
 
-  /**
-   * @return int
-   */
-  public int getEstadoDeTarjeta (int tema) {
-      return tema;
-  }
+    /**
+    * @return int
+    */
+    public int getEstadoDeTarjeta (int tema) {
+        return tema;
+    }
 
 
-  /**
-   * @param tema
-   */
-  public void setEstadoDeTarjeta (int tema) {
-  } 
+    /**
+    * @param tema
+    */
+    public void setEstadoDeTarjeta (int tema) {
+    
+    } 
 }
