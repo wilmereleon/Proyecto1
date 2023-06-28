a0a47092cf9a76f98d6ef937fa7da4a0663d1ac2
Wílmer E. León Díaz <42726764+wilmereleon@users.noreply.github.com>
2023-06-28 12:29:42 -0500

    Creación de matriz en la clase Tablero
    Se crea el código que permite la creación de la matriz del tablero junto con el audio (revisar error en línea de mezcla del arreglo).

--- src/javaapplication1/Tablero.java
+++ src/javaapplication1/Tablero.java
@@ -4,6 +4,10 @@
  */
 package javaapplication1;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import javafx.scene.media.AudioClip;
+
 /**
 *
 * @author Wilmer E Leon
@@ -13,61 +17,106 @@ public class Tablero {
   // Fields
   //
    
-  private Tablero tablero; // El modelo del tablero
-  private int nivel; // El nivel del tablero
-  private int numero = 1;
-  private Tarjeta[] tarjetas;
+  //private Tablero tablero; // El modelo del tablero
+  //private int nivel; // El nivel del tablero
+  //private int numero = 1;
+  private Tarjeta[][] tarjetas;
+  private int filas;
+  private int columnas;
   
   
     
   //
   // Constructors
   //
-  public Tablero () {
-  
+  /**
+   * 
+   * @author Wilmer E Leon
+   * @param nivel 
+   * COmentario: recibe el nivel para determinar la cantidad de filas y columnas.
+   */
+  public Tablero (int nivel) {
+      /**
+       * Comentario: Calcula el número de filas y columnas del tablero, según el
+       * nivel.
+       */
+      filas = nivel + 3;
+      columnas = nivel + 3;
+      
+      /**
+       * Comentario: Inicializa el arreglo de tarjetas con el tamaño adecuado.
+       */
+      tarjetas = new Tarjeta [filas][columnas];
+      
+      /**
+       * Comentario: Inicializa el arreglo de pares con los números del 0 al 
+       * (filas * columnas / 2) - 1 repetidos dos veces.
+       */
+      int [] parejas = new int [filas * columnas];
+      for (int i = 0; i < parejas.length / 2; i++) {
+          parejas [i] = i;
+          parejas [i + parejas.length / 2] = i;
+      }
+      
+      /**
+       * Mezcla el arreglo de pares para que queden en orden aleatorio.
+       * Revisar el error en la documentación de la librería.
+       */
+      //Collections.suffle(new ArrayList<Integer>(parejas));
+      
+      /**
+       * Recorre el arreglo de tarjetas y crear cada una con sus propiedades.
+       */
+      for (int i = 0; i < filas; i++) {
+          for (int j = 0; j < columnas; j++) {
+              
+              /**
+               * Comentario: crea un nuevo sonido a partir de una archivo wav y 
+               * lo asigna al arreglo de sonidos en la posición correspondiente.
+               */
+              AudioClip sonido = new AudioClip(getClass().getResource("sonido" + parejas [i * columnas + j] + "TheReturn.mp3").toString());
+              
+              /**
+               * Crear una nueva tarjeta con el sonido y el par correspondiente
+               * y asignarla al array de tarjetas en la posición correspondiente.
+               */
+              tarjetas [i][j] = new Tarjeta (sonido, parejas [i * columnas + j]);
+          }
+      }
   }
   
   //
   // Methods
   //
-
+  /**
+   * 
+   * @return filas
+   */
+  public int getFilas () {
+      return filas;
+  }
+  
+  /**
+   * 
+   * @return columnas
+   */
+  public int getColumnas () {
+      return columnas;
+  }
 
   //
   // Accessor methods
   //
-
   /**
-   * Obtiene el valor de numero.
-   * @param numero , el nuevo valor de numero.
+   * 
+   * @param fila
+   * @param columna
+   * @return tarjetas con fila y columna
    */
-  public void setNumero (int numero) {
-    numero = numero;
+  public Tarjeta getTarjeta (int fila, int columna) {
+      return tarjetas [fila][columna];
   }
-
-  /**
-  * Establece el valor de numero.
-  * @return el valor de numero.
-  */
-  public int getNumero (int numero) {
-    return numero;
-  }
-
-  /**
-  * Obtiene el valor de tarjetas.
-  * @param tarjetas , el nuevo valor de tarjetas.
-  */
-  public void setTarjetas () {
-    
-  }
-
-  /**
-  * Establece el valor de tarjetas.
-  * @return el valor de tarjetas.
-  */
-  public Tarjeta[] getTarjetas (Tarjeta [] tarjetas) {
-    return tarjetas;
-  }
-
+  
   //
   // Other methods
   //
